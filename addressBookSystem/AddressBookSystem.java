ef3ad6a8c804802cd66de8dbc1e4e8e98c4e1613
tejaswini_14 <shelaketejaswini14@gmail.com>
2024-09-02 15:41:49 +0530

    Added: Add new Contact to address book

--- addressBookSystem/AddressBookSystem.java
+++ addressBookSystem/AddressBookSystem.java
@@ -5,8 +5,35 @@ public class AddressBookSystem {
 	
 	private List<Contact> contacts = new ArrayList<>();
 	
+	private static AddressBookSystem addressBook = new AddressBookSystem();
+	
+	static Scanner scanner = new Scanner(System.in);
+			
 	public void addContacts(Contact contact) {
 		contacts.add(contact);
+	}
+	
+	 private static void addNewContact() {
+		 System.out.print("Enter First Name: ");
+	     String firstName = scanner.nextLine();
+	     System.out.print("Enter Last Name: ");
+	     String lastName = scanner.nextLine();
+	     System.out.print("Enter Address: ");
+	     String address = scanner.nextLine();
+	     System.out.print("Enter City: ");
+	     String city = scanner.nextLine();
+	     System.out.print("Enter State: ");
+	     String state = scanner.nextLine();
+	     System.out.print("Enter Zip: ");
+	     String zip = scanner.nextLine();
+	     System.out.print("Enter Phone Number: ");
+	     String phoneNumber = scanner.nextLine();
+	     System.out.print("Enter Email: ");
+	     String email = scanner.nextLine();
+
+	     Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+	     addressBook.addContacts(contact);
+	     System.out.println("Contact added successfully.");
 	}
 	
 	public void printContacts() {
@@ -14,22 +41,34 @@ public class AddressBookSystem {
 			System.out.println(contact);
 		}
 	}
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book System Program");		
+		System.out.println("Welcome to Address Book System Program");	
 		
-		AddressBookSystem addressBook = new AddressBookSystem();
 		
-		Contact contact = new Contact(
-				"Teja",
-				"Shelake",
-				"Erandwane",
-				"Pune",
-				"Maharastra",
-				"123456",
-				"1234567890",
-				"abc@gmail.com"
-				);
-		addressBook.addContacts(contact);
-		addressBook.printContacts();
+		while (true) {
+            System.out.println("1. Add New Contact");
+            System.out.println("2. Display Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); 
+
+            switch (choice) {
+                case 1:
+                    addNewContact();
+                    break;
+                case 2:
+                    addressBook.printContacts();
+                    break;
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+		
+		
 	}
 }
